c70a5cebbe039d4852e4208ba2c177689ed9e900
Ozan <ozannkaranfill@gmail.com>
2024-09-14 14:16:08 +0300

    Initial Project Files

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright OzanKaranfil.
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright OzanKaranfil.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
